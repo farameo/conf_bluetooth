4cd8e312599c469ea5dbb9fd3756292cd5426628
farameo <farameo@hotmail.com>
2017-08-26 08:20:30 -0300

    final. configurar bluetooth con explicaciones

--- conf_bluetooth.ino
+++ conf_bluetooth.ino
@@ -1,25 +1,27 @@
+
 #include <SoftwareSerial.h>
+#define BT_RT 10
+#define BT_TX 11
+#define BTVCC 9
+#define BTKEY 8
 
-#define rx 10
-#define tx 11
+SoftwareSerial Bluetooth(BT_TX, BT_RT); // RX, TX
 
-SoftwareSerial mySerial(rx, tx);
 
 void setup() {
-  
   Serial.begin(9600);
-  Serial.println("Listo!");
-
-  mySerial.begin(38400);
-  mySerial.println("listo blueetooth");
+  pinMode(BTVCC, OUTPUT);                         // Marca el PIN de alimentacion para el HC-05 como salida
+  pinMode(BTKEY, OUTPUT);                          // Marca el PIN del modo AT para el HC-05 como salida
+  digitalWrite(BTKEY, HIGH);                         // Activa el modo AT Full
+  delay (500) ;                                           // Espera antes de encender el modulo
+  Serial.println("Alimentando el modulo HC-05");
+  digitalWrite (BTVCC, HIGH);                       // Enciende el modulo
+  //delay(600); digitalWrite (BTKEY, LOW);      // Cambia al modo AT Mini
+  Serial.println("Esperando comandos AT:");
+  Bluetooth.begin(38400); 
 }
 
-void loop() { // run over and over
-  if (mySerial.available()) {
-    Serial.write(mySerial.read());
-  }
-  if (Serial.available()) {
-    mySerial.write(Serial.read());
-  }
+void loop(){
+  if (Bluetooth.available()){ Serial.write(Bluetooth.read()); }
+  if (Serial.available()){ Bluetooth.write(Serial.read()); }
 }
-
